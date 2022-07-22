45ec106d24c43762e22a5674c6d076e56f86f455
Eva Sennrich <evasennrich@gmail.com>
2022-07-22 15:07:02 -0700

    setup the skeleton for the project

---
+++ avent-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button>HELLO WORLD</Button>
+    </div>
+  );
+}
+
+export default App;
