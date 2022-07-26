832dc4b88e18e4efd2c1fc2fae3a80fe991f4f04
David Barcenas <dbarcenas@dbarcen-ltmc97g.internal.salesforce.com>
2022-07-25 17:27:54 -0700

    app.jsx landing route, login route, and register route were added

--- avent-app/src/App.jsx
+++ avent-app/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./components/Landing";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
@@ -8,11 +11,11 @@ function App() {
         <BrowserRouter>
           <main>
             <Routes>
-              {/* <Route path="/register" element={<Register />}></Route>
-              <Route path="/login" element={<Login />}></Route> */}
+              <Route path="/register" element={<Register />}></Route>
+              <Route path="/login" element={<Login />}></Route>
 
               {/*Landing page routes*/}
-
+              <Route path="/" element={<Landing />} />
               {/*Event feed routes*/}
             </Routes>
           </main>
