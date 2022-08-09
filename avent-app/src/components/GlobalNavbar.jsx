918c8106128521c01bbba60d666194fcc242d046
David Barcenas <dbarcenas@dbarcen-ltmc97g.internal.salesforce.com>
2022-08-09 15:27:47 -0700

    small change in global navbar

--- avent-app/src/components/GlobalNavbar.jsx
+++ avent-app/src/components/GlobalNavbar.jsx
@@ -2,7 +2,16 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Avatar, Stack, Box, Container, Link, Menu, MenuItem, IconButton } from "@mui/material";
+import {
+  Avatar,
+  Stack,
+  Box,
+  Container,
+  Link,
+  Menu,
+  MenuItem,
+  IconButton,
+} from "@mui/material";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
@@ -30,9 +39,7 @@ export default function GlobalNavbar({ isLoggedIn, setIsLoggedIn, setUser }) {
    * Function that handles when the user logs out
    */
   const handleOnLogout = () => {
-    // setIsLoggedIn(false);
     apiClient.deleteToken();
-    // setUser({});
     navigate("/");
   };
 
@@ -53,12 +60,22 @@ export default function GlobalNavbar({ isLoggedIn, setIsLoggedIn, setUser }) {
                 fontSize: 20,
               }}
             >
-              <Link to="/feed" color="secondary" component={RouterLink} underline="none">
+              <Link
+                to="/feed"
+                color="secondary"
+                component={RouterLink}
+                underline="none"
+              >
                 Avent
               </Link>
             </Typography>
             <Box sx={{ flexGrow: 0 }}>
-              <Stack direction="row" spacing={3} justifyContent="center" alignItems="center">
+              <Stack
+                direction="row"
+                spacing={3}
+                justifyContent="center"
+                alignItems="center"
+              >
                 <IconButton component={RouterLink} to="/createEvent">
                   <AddCircleOutlineIcon
                     style={{ cursor: "pointer" }}
