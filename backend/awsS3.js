d9dd42659c2647ceed85ce30fb7a550f3d84c745
Daniel O <danielortiz6810@gmail.com>
2022-08-11 15:10:59 -0700

    added stuff

--- backend/awsS3.js
+++ backend/awsS3.js
@@ -4,7 +4,7 @@ const S3 = require('aws-sdk/clients/s3')
 
 const bucketName = process.env.AWS_BUCKET_NAME
 const region = process.env.AWS_BUCKET_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY
+const accessKeyId = process.env.AWS_USER_ACCESS_KEY
 const secretAccessKey = process.env.AWS_SECRET_KEY
 
 const s3 = new S3({
