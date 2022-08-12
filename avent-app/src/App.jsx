4e3f26c0e0897f948ac168d6cafdee306d36584d
Eva Sennrich <evasennrich@gmail.com>
2022-08-12 10:59:07 -0700

    app.jsx prettier extn changes

--- avent-app/src/App.jsx
+++ avent-app/src/App.jsx
@@ -18,7 +18,6 @@ import ProfileDetail from "./components/ProfileDetail";
 
 import PWGenerate from "./components/TESTPW/PWGenerate";
 
-
 function App() {
   const [user, setUser] = useState({});
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -40,115 +39,31 @@ function App() {
         <BrowserRouter>
           <main>
             <Routes>
-              <Route
-                path="/register"
-                element={
-                  <Register
-                    user={user}
-                    setUser={setUser}
-                    isLoggedIn={isLoggedIn}
-                    setIsLoggedIn={setIsLoggedIn}
-                  />
-                }
-              ></Route>
-              <Route
-                path="/login"
-                element={
-                  <Login
-                    user={user}
-                    setUser={setUser}
-                    isLoggedIn={isLoggedIn}
-                    setIsLoggedIn={setIsLoggedIn}
-                  />
-                }
-              ></Route>
+              <Route path="/register" element={<Register user={user} setUser={setUser} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />}></Route>
+              <Route path="/login" element={<Login user={user} setUser={setUser} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />}></Route>
               {/*Landing page routes*/}
 
               <Route path="/" element={<Landing />} />
 
               {/*Event feed routes*/}
 
+              <Route path="/feed" element={<EventFeed setUser={setUser} isLoggedIn={isLoggedIn.user} />} />
+              <Route path="/details/:eventId" element={<EventDetails user={user} />} />
 
-              <Route
-                path="/feed"
-                element={
-                  <EventFeed setUser={setUser} isLoggedIn={isLoggedIn.user} />
-                }
-              />
-              <Route
-                path="/details/:eventId"
-                element={<EventDetails user={user} />}
-              />
-
-              <Route
-                path="/createEvent"
-                element={<CreateEvent user={user} />}
-              />
+              <Route path="/createEvent" element={<CreateEvent user={user} />} />
 
               <Route path="/aboutUs" element={<AboutUs />} />
-              <Route
-                path="/settings/profile"
-                element={<Profile user={user} setUser={setUser} />}
-              />
-              <Route
-                path="/settings/reservations"
-                element={<Reservations user={user} />}
-              />
-              <Route
-                path="/settings/listings"
-                element={<Listings user={user} />}
-              />
+              <Route path="/settings/profile" element={<Profile user={user} setUser={setUser} />} />
+              <Route path="/settings/reservations" element={<Reservations user={user} />} />
+              <Route path="/settings/listings" element={<Listings user={user} />} />
               <Route path="*" element={<NotFound />} />
 
-              <Route
-                path="/feed"
-                element={
-                  <EventFeed
-                    isLoggedIn={isLoggedIn}
-                    setIsLoggedIn={setIsLoggedIn}
-                  />
-                }
-              />
-              <Route
-                path="/details/:eventId"
-                element={
-                  <EventDetails
-                    isLoggedIn={isLoggedIn}
-                    setIsLoggedIn={setIsLoggedIn}
-                  />
-                }
-              />
-              <Route
-                path="/createEvent"
-                element={
-                  <CreateEvent
-                    isLoggedIn={isLoggedIn}
-                    setIsLoggedIn={setIsLoggedIn}
-                  />
-                }
-              />
-              <Route
-                path="/aboutUs"
-                element={
-                  <AboutUs
-                    isLoggedIn={isLoggedIn}
-                    setIsLoggedIn={setIsLoggedIn}
-                  />
-                }
-              />
-              <Route
-                path="*"
-                element={
-                  <NotFound
-                    isLoggedIn={isLoggedIn}
-                    setIsLoggedIn={setIsLoggedIn}
-                  />
-                }
-              />
-              <Route
-                path="/profiles/:userId"
-                element={<ProfileDetail user={user} />}
-              />
+              <Route path="/feed" element={<EventFeed isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+              <Route path="/details/:eventId" element={<EventDetails isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+              <Route path="/createEvent" element={<CreateEvent isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+              <Route path="/aboutUs" element={<AboutUs isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+              <Route path="*" element={<NotFound isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />
+              <Route path="/profiles/:userId" element={<ProfileDetail user={user} />} />
               <Route path="/profile/:userId" element={<ProfileDetail user={user} />} />
               <Route path="/pw" element={<PWGenerate />} />
             </Routes>
